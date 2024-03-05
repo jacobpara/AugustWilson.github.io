94f70a096b4ba8ca5d96500f955e03fe75fab8b9
jacobpara <144147592+jacobpara@users.noreply.github.com>
2024-03-04 21:21:36 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,9 @@
+function showTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "block";
+}
+
+function hideTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "none";
+}
